90b6f18688a234f4f45a195d8a567e6d2da0e71d
Chandana Datta Uppalapati <chandan.datta.cd@gmail.com>
2016-10-12 15:06:54 -0500

    Fix the bug in appstart to get the Private IP for the instance

--- featurerequest-cookbook/recipes/appstart.rb
+++ featurerequest-cookbook/recipes/appstart.rb
@@ -3,6 +3,6 @@ bash "start_appserver" do
 	cwd '/home/www'
 	code <<-EOL
 		source env/bin/activate
-		gunicorn run_production:app --reload -b #{node["opsworks"]["instance"]["private_ip"]}:8000
+		gunicorn run_production:app --reload -b #{node["opsworks"]["instance"]["PrivateIp"]}:8000
 	EOL
 end
