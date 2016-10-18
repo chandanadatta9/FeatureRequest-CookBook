b2401a78a6de54eb5ad8d0d2df8211a72be82dc9
Chandana Datta Uppalapati <chandan.datta.cd@gmail.com>
2016-10-18 02:19:20 -0500

    Add correct full file name

--- featurerequest-cookbook/recipes/appclean.rb
+++ featurerequest-cookbook/recipes/appclean.rb
@@ -1,4 +1,4 @@
-file '/etc/init/featurerequest' do
+file '/etc/init/featurerequest.conf' do
 	action :delete
-	only_if { File.exist? '/etc/init/featurerequest'}
+	only_if { File.exist? '/etc/init/featurerequest.conf'}
 end
